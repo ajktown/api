307aaac3c374a4fb09ce08ae1aec54f4df14475d
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-02-28 05:05:19 +0900

    feat: port 8000

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(8000) // TODO: Use the env data
 }
 bootstrap();
