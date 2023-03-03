eb0634f76b57807c8f57a5939d2098d07012e20f
Jeongwoo Kim <jkim67cloud@gmail.com>
2023-03-04 05:26:33 +0900

    todo: removed

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,7 +5,6 @@ import { NextFunction, Request, Response } from "express"
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  // TODO: Can we use that @Request stuff here?
   async use (req: Request, res: Response, next: NextFunction) {
      // TODO: Temporary, wait for 1 second
     console.log("passing middleware...")
