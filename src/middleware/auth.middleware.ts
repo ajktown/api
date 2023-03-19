c3d815f563fc2ba4479441eef23df7f4319b4d60
AJ Kim <jkim67cloud@gmail.com>
2023-03-20 06:00:09 +0900

    feat: small

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -5,6 +5,7 @@ import { NextFunction, Request, Response } from 'express'
 export class AuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     // TODO: Temporary, wait for 1 second
+    // TODO: Apply access control for token, in a standard Nest JS Way with validator
     await new Promise((resolve) => setTimeout(resolve, 1000))
 
     next()
