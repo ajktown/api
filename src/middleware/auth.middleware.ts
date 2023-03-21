421df6df8a32452f96e26b6844171bd45c1451de
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2023-03-21 17:03:14 +0900

    refactor: var out of the function

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,12 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
 
+// Creates wait time for n milliseconds to mock production
+// TODO: This should happen only for the ENV !== "prod"
+const PRIVATE_WAITING_MILLISECONDS = 500
+
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     // TODO: Temporary, wait for 1 second
     // TODO: Apply access control for token, in a standard Nest JS Way with validator
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+    await new Promise((resolve) => setTimeout(resolve, PRIVATE_WAITING_MILLISECONDS))
 
     next()
   }
