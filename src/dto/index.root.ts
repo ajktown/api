aa3767b8fd79557a1fb798b2464296c435047bc5
AJ Kim <jkim67cloud@gmail.com>
2023-04-13 09:40:21 +0900

    feat: todo

--- src/dto/index.root.ts
+++ src/dto/index.root.ts
@@ -1,5 +1,7 @@
 import { IsNumber, IsOptional } from 'class-validator'
 
+// TODO: The limit has to be set by default if not given, for less traffics (i.e 1000).
+
 export class GetReqDTORoot {
   @IsNumber()
   @IsOptional()
