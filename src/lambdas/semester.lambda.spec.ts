a7b150df15fa13d4abe9867b9672e80e55cf906e
AJ Kim <jkim67cloud@gmail.com>
2023-05-26 04:44:09 +0900

    feat: semester lambda

--- src/lambdas/semester.lambda.spec.ts
+++ src/lambdas/semester.lambda.spec.ts
@@ -1,8 +1,9 @@
-import { getSemesterByDataLambda } from './get-semester-by-date.lambda'
+import { semesterLambda } from './semester.lambda'
 
-describe(`getSemesterByDataLambda(date: Date)`, () => {
+describe(`semesterLambda.fromDate()`, () => {
   it(`should be exposed as a function`, () => {
-    expect(getSemesterByDataLambda).toBeDefined()
+    expect(semesterLambda).toBeDefined()
+    expect(semesterLambda.fromDate).toBeDefined()
   })
 
   interface Test {
@@ -75,8 +76,54 @@ describe(`getSemesterByDataLambda(date: Date)`, () => {
 
   tests.forEach((test) => {
     it(`should return "${test.wantSemester}" with arg(s) "${test.sampleDateStr}"`, () => {
-      const gotSemester = getSemesterByDataLambda(new Date(test.sampleDateStr))
+      const gotSemester = semesterLambda.fromDate(new Date(test.sampleDateStr))
       expect(gotSemester).toBe(test.wantSemester)
+    })
+  })
+})
+
+describe(`semesterLambda.toYearAndQuarter()`, () => {
+  it(`should be exposed as a function`, () => {
+    expect(semesterLambda).toBeDefined()
+    expect(semesterLambda.toYearAndQuarter).toBeDefined()
+  })
+
+  interface Test {
+    sampleSemester: number
+    wantYear: number
+    wantQuarter: number
+  }
+
+  const tests: Test[] = [
+    {
+      sampleSemester: 231,
+      wantYear: 2023,
+      wantQuarter: 1,
+    },
+    {
+      sampleSemester: 242,
+      wantYear: 2024,
+      wantQuarter: 2,
+    },
+    {
+      sampleSemester: 243,
+      wantYear: 2024,
+      wantQuarter: 3,
+    },
+    {
+      sampleSemester: 244,
+      wantYear: 2024,
+      wantQuarter: 4,
+    },
+  ]
+
+  tests.forEach((test) => {
+    it(`should return "${test.wantYear}", "${test.wantQuarter}" with arg(s) "${test.sampleSemester}"`, () => {
+      const gotYearQuarter = semesterLambda.toYearAndQuarter(
+        test.sampleSemester,
+      )
+      expect(gotYearQuarter.year).toBe(test.wantYear)
+      expect(gotYearQuarter.quarter).toBe(test.wantQuarter)
     })
   })
 })
---
+++ src/lambdas/semester.lambda.ts
@@ -0,0 +1,14 @@
+export const semesterLambda = {
+  /** Returns semester value with given date in UTC standard rather than local time that server runs. */
+  fromDate: (givenDate: Date) => {
+    const year = (givenDate.getUTCFullYear() - 2000) * 10 // i.e) 23: Year of 2023
+    const quarter = Math.floor(givenDate.getUTCMonth() / 3) + 1 // i.e) 1 ~ 4
+    return year + quarter
+  },
+  /** Returns year and quarter of the given semester */
+  toYearAndQuarter: (semester: number) => {
+    const year = Math.floor(semester / 10) + 2000
+    const quarter = semester % 10
+    return { year, quarter }
+  },
+}
