08f58ea8cfa197f3b028b0487e79116bd2371edc
AJ Kim <jkim67cloud@gmail.com>
2023-06-10 08:08:53 +0900

    feat: comment out the goal

--- src/responses/get-who-am-i.res.ts
+++ src/responses/get-who-am-i.res.ts
@@ -9,11 +9,13 @@ interface GetAuthPrepResInfo {
 interface PrivateGetAuthPrepResYesSignedIn extends GetAuthPrepResInfo {
   isSignedIn: true
   signedInUserInfo: IOauthPayload
+  // profilePictureUrl: string
 }
 
 interface PrivateGetAuthPrepResNoSignedIn extends GetAuthPrepResInfo {
   isSignedIn: false
   signedInUserInfo: null
+  // profilePictureUrl: null
 }
 
 export type GetAuthPrepRes =
