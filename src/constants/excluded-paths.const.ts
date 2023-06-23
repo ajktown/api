7240bf437c0b2fda86fa2e3db84da312db1df54a
AJ Kim <jkim67cloud@gmail.com>
2023-06-23 18:54:14 +0900

    feat: more comments

--- src/constants/excluded-paths.const.ts
+++ src/constants/excluded-paths.const.ts
@@ -4,15 +4,19 @@ import { AjkTownApiVersion } from '@/controllers/index.interface'
 import { RequestMethod } from '@nestjs/common'
 import { RouteInfo } from '@nestjs/common/interfaces'
 
+/**
+ * All of the paths that should be excluded from auth middleware.
+ */
+
 type PathAndMethod = [string, RequestMethod]
 
-// Does not have /api/v1 paths
-const privateExcludedPaths: PathAndMethod[] = [
+/** Root paths do not contain basic prefix like /api/v1 unlike other api paths. */
+const privateRootPaths: PathAndMethod[] = [
   [ApiHomePath.Home, RequestMethod.GET],
   [ApiHomePath.HomeHelloWorld, RequestMethod.GET],
 ]
 
-// Does have /api/v1 prefix
+// API First version paths
 const privateV1Paths: PathAndMethod[] = [
   [AuthControllerPath.PostSignOut, RequestMethod.POST],
   [AuthControllerPath.GetAuthPrep, RequestMethod.GET],
@@ -25,6 +29,6 @@ const getV1Path = ([path, method]: PathAndMethod): PathAndMethod => {
 }
 
 export const excludedPaths: RouteInfo[] = [
-  ...privateExcludedPaths,
+  ...privateRootPaths,
   ...privateV1Paths.map(getV1Path),
 ].map(([path, method]) => ({ path, method }))
