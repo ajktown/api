de357fc5dfe2ceda20b3fbb934b744c596b36024
AJ Kim <jkim67cloud@gmail.com>
2023-06-27 05:54:16 +0900

    feat: params with transform

--- src/dto/index.validator.ts
+++ src/dto/index.validator.ts
@@ -1,14 +1,13 @@
 import { TransformFnParams } from 'class-transformer'
 
-// TODO: Do not use any. use the TransformFnParams
 /** Convert into boolean. Use ONLY after @Transform(intoBoolean) */
-export const intoBoolean = ({ value }: any): boolean => {
+export const intoBoolean = ({ value }: TransformFnParams): boolean => {
   if (value === true || value === 'true') return true
   if (value === false || value === 'false') return false
   throw new Error('Invalid boolean value')
 }
 
-export const intoNumber = ({ value }: any): number => {
+export const intoNumber = ({ value }: TransformFnParams): number => {
   const num = Number(value)
   if (!isNaN(num)) return num
   throw new Error('Invalid number value')
@@ -24,7 +23,7 @@ export const intoNumberWithLimit = (params: {
   return converted
 }
 
-export const intoArray = ({ value }: any): string[] => {
+export const intoArray = ({ value }: TransformFnParams): string[] => {
   if (typeof value === 'string') return [value]
   if (Array.isArray(value)) return value
   throw new Error('Invalid tags value')
