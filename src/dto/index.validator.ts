456ab9be566a3cbcd1fa4a29de2e98caa3efaadb
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2023-06-28 08:08:26 +0900

    Update src/dto/index.validator.ts

--- src/dto/index.validator.ts
+++ src/dto/index.validator.ts
@@ -19,7 +19,7 @@ export const intoNumberWithMaxLimit = (params: {
 }): number => {
   const converted = intoNumber(params.transformFnParams)
   if (params.maxLimit < converted)
-    throw new Error(`Invalid number value. Max: ${params.maxLimit}`)
+    throw new Error(`Exceeded max limit. Got: ${converted} Max Limit: ${params.maxLimit}`)
   return converted
 }
 
