e16c337b83c261f0216947e3cadcc277ff5589a3
AJ Kim <jkim67cloud@gmail.com>
2023-09-27 08:35:21 +0900

    doc: res

--- src/responses/get-words.res.ts
+++ src/responses/get-words.res.ts
@@ -3,7 +3,10 @@ import { IWord } from '@/domains/word/index.interface'
 import { PaginationRoot } from '@/handlers/get-pagination.handler'
 
 export interface GetWordsRes extends PaginationRoot {
+  // semester data with given semester
   semester: Partial<ISemester> | undefined
+  // word ids within the semester
   wordIds: string[]
+  // actual word contents within the semester
   words: Partial<IWord>[]
 }
