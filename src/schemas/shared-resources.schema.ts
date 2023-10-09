245bad3c2e9d34b35dd73dc3e4e284a9aab17190
AJ Kim <jkim67cloud@gmail.com>
2023-10-09 12:43:35 +0900

    feat: shared pool

---
+++ src/schemas/shared-resources.schema.ts
@@ -0,0 +1,33 @@
+import { DataBasicsDate } from '@/global.interface'
+import { ModelDefinition, Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument, Model } from 'mongoose'
+import {
+  defaultSchemaTimestampsConfig,
+  SchemaCollectionName,
+} from './index.collections'
+
+export type SharedResourceDoc = HydratedDocument<SharedResourceProps, DataBasicsDate>
+
+export type SharedResourceModel = Model<SharedResourceDoc>
+
+@Schema({
+  collection: SchemaCollectionName.SharedResources,
+  timestamps: defaultSchemaTimestampsConfig,
+})
+export class SharedResourceProps {
+  @Prop({ required: true })
+  ownerID: string // the owner id 5f85729......
+
+  @Prop()
+  resourceType: string // word
+
+  @Prop()
+  wordId: string
+}
+
+export const SharedResourceSchema = SchemaFactory.createForClass(SharedResourceProps)
+
+export const sharedResourceModelDefinition: ModelDefinition = {
+  name: SharedResourceProps.name,
+  schema: SharedResourceSchema,
+}
