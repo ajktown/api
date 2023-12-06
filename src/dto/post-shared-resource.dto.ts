d46b8986abc8e6035c027f2cec5709b2f262a4c2
AJ Kim <jkim67cloud@gmail.com>
2023-12-06 10:12:16 +0900

    doc: basic explanation for expireAfterSecs

--- src/dto/post-shared-resource.dto.ts
+++ src/dto/post-shared-resource.dto.ts
@@ -1,6 +1,13 @@
 import { IsNumber, IsOptional, IsString } from 'class-validator'
 
 export class PostSharedResourceDTO {
+  /**
+   * You may set up the expiring time.
+   * If you want your shared resource to last for 24 hours,
+   * set expireAfterSecs = 60 * 60 * 24 = 86,400.
+   *
+   * At this point it must expire
+   */
   @IsOptional()
   @IsNumber()
   expireAfterSecs: number
