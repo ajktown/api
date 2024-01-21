35a73531ef355438ab8a2f9abda85813c1dd938c
AJ Kim <jkim67cloud@gmail.com>
2024-01-22 07:01:19 +0900

    feat: returns clean format

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common'
+import { Request, Router } from 'express'
 
 @Injectable()
 export class AppService {
+  getHome(req: Request) {
+    const router = req.app._router as Router
+
+    return {
+      message: 'Welcome to AJK Town API Service!',
+      websites: [
+        'https://wordnote.ajktown.com',
+        'https://consistency.ajktown.com',
+      ],
+      endpoints: router.stack
+        .map((layer) => {
+          if (layer.route) {
+            // To make the output looks better, we padEnd the method name with spaces.
+            // DELETE is the longest method name in standard HTTP methods.
+            // "GET"    => "GET   "
+            // "POST"   => "POST  "
+            // "PUT"    => "PUT   "
+            // "PATCH"  => "PATCH "
+            // "DELETE" => "DELETE"
+            const method = layer.route?.stack[0].method
+              .toUpperCase()
+              .padEnd(6, ' ')
+            const path = layer.route?.path
+            return `${method} ${path}`
+          }
+        })
+        .filter((item) => item),
+    }
+  }
   getHello(): string {
     return 'Hello World from AJK Town API Service! Check out https://wordnote.ajktown.com'
   }
