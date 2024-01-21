9e35f9b20e85de448ee661c843f7b1653ceeceda
AJ Kim <jkim67cloud@gmail.com>
2024-01-21 21:20:30 +0900

    feat: IAction

---
+++ src/domains/action/index.interface.ts
@@ -0,0 +1,8 @@
+import { DataBasicsDate } from 'src/global.interface'
+
+export interface IAction extends DataBasicsDate {
+  id: string
+  ownerID: string
+  groupId: string
+  level: number // 1 ~ 4 only
+}
