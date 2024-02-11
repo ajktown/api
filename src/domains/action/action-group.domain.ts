2dd1e6d5702f9b8fe443a143063b6005b7041137
AJ Kim <jkim67cloud@gmail.com>
2024-02-11 12:05:00 +0900

    feat: apply getYYYYMMDD for the rest

--- src/domains/action/action-group.domain.ts
+++ src/domains/action/action-group.domain.ts
@@ -4,16 +4,24 @@ import { AccessTokenDomain } from '../auth/access-token.domain'
 import { WordChunkDomain } from '../word/word-chunk.domain'
 import { ActionGroupFixedIdSuffix } from '@/constants/action-group.const'
 import { WordDomain } from '../word/word.domain'
+import { timeHandler } from '@/handlers/time.handler'
 
 export class ActionGroupDomain extends DomainRoot {
+  private readonly isTodayHandled: boolean
   private readonly totalCount: number
   private readonly domains: ActionDomain[]
 
-  private constructor(domains: ActionDomain[]) {
+  private constructor(atd: AccessTokenDomain, domains: ActionDomain[]) {
     super()
     this.domains = domains
+    // isTodayHandled
+    this.isTodayHandled = domains.some(
+      (d) =>
+        timeHandler.getYYYYMMDD(d.toResDTO().createdAt, atd.timezone) ===
+        timeHandler.getYYYYMMDD(new Date(), atd.timezone),
+    )
     // total counts is number of actions committed that is at least level 1 or higher
-    this.totalCount = domains.filter(d => d.toResDTO().level).length
+    this.totalCount = domains.filter((d) => d.toResDTO().level).length
   }
 
   static fromWordChunk(
@@ -25,9 +33,10 @@ export class ActionGroupDomain extends DomainRoot {
     // 2024 only, create an empty action domains
     const dateWordDomainMap = new Map<string, WordDomain>() // i.e) 2024-01-01 => WordDomain
     for (const wordDomain of wordChunk.wordDomains) {
-      const date = new Date(wordDomain.toResDTO(atd).dateAdded)
-        .toISOString()
-        .split(`T`)[0] // i.e) 2024-01-01
+      const date = timeHandler.getYYYYMMDD(
+        wordDomain.toResDTO(atd).dateAdded,
+        atd.timezone,
+      )
       dateWordDomainMap.set(date, wordDomain)
     }
 
@@ -37,7 +46,7 @@ export class ActionGroupDomain extends DomainRoot {
     const actionDomains: ActionDomain[] = []
 
     for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
-      const date = d.toISOString().split(`T`)[0] // i.e) 2024-01-01
+      const date = timeHandler.getYYYYMMDD(d, atd.timezone)
       if (dateWordDomainMap.has(date)) {
         actionDomains.push(
           dateWordDomainMap.get(date).toActionDomain(atd, groupId),
@@ -47,6 +56,6 @@ export class ActionGroupDomain extends DomainRoot {
       }
     }
 
-    return new ActionGroupDomain(actionDomains)
+    return new ActionGroupDomain(atd, actionDomains)
   }
 }
