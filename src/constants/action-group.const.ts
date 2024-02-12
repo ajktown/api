68093417601a5d878b6981059d548009520ac118
AJ Kim <jkim67cloud@gmail.com>
2024-02-13 07:50:47 +0900

    feat: ActionGroupFixedNames

--- src/constants/action-group.const.ts
+++ src/constants/action-group.const.ts
@@ -2,3 +2,8 @@
 export enum ActionGroupFixedIdSuffix {
   PostWordConsistency = '_post-word-consistency',
 }
+
+// The name here is reserved and cannot be changed once released to the production.
+export enum ActionGroupFixedNames {
+  PostWordConsistency = 'post-word-consistency',
+}
