0e51073c3a8632ab7c37b76a5eec2b80649c54b3
AJ Kim <jkim67cloud@gmail.com>
2024-03-02 10:57:58 +0900

    feat: userModelDefinition and  UserService

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -3,10 +3,17 @@ import { RitualService } from '@/services/ritual.service'
 import { actionGroupModelDefinition } from '@/schemas/action-group.schema'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UserController } from '@/controllers/user.controller'
+import { userModelDefinition } from '@/schemas/deprecated-user.schema'
+import { UserService } from '@/services/user.service'
 
 @Module({
-  imports: [MongooseModule.forFeature([actionGroupModelDefinition])],
+  imports: [
+    MongooseModule.forFeature([
+      userModelDefinition,
+      actionGroupModelDefinition,
+    ]),
+  ],
   controllers: [UserController],
-  providers: [Logger, RitualService],
+  providers: [Logger, UserService, RitualService],
 })
 export class UserModule {}
