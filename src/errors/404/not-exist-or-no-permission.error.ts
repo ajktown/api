1bc10747a964528ed5c68771a8d61cb67615d03c
Akane Oba <aka1105maron@gmail.com>
2024-03-05 20:15:50 +0900

    fix: create class and change class to extend

--- src/errors/404/not-exist-or-no-permission.error.ts
+++ src/errors/404/not-exist-or-no-permission.error.ts
@@ -1,11 +1,11 @@
-import { BadRequestError } from '../../400/index.error'
+import { NotFoundError } from './index.error'
 
 /** Thrown when a certain resource is not found or the user does not have permission to access it.
  * This is used, especially when you want to hide its existence from the user.
  * @example
  * throw new NotExistOrNoPermissionError()
  */
-export class NotExistOrNoPermissionError extends BadRequestError {
+export class NotExistOrNoPermissionError extends NotFoundError {
   constructor() {
     super(
       `The resource does not exist or you do not have permission to access it`,
