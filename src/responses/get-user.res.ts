6d13c06ff69207e084a024cc56b1ab91d296c00f
Aaron Jeongwoo Kim <53258958+mlajkim@users.noreply.github.com>
2024-04-30 06:13:05 +0900

    Display name (#119)
    # Background
    Implement displayname
    
    ## TODOs
    - [x] Implement displayname
    
    
    ## Checklist Before PR Review
    - [x] The following has been handled:
      -  `Draft` is set for this PR
      - `Title` is checked
      - `Background` is filled
      - `TODOs` are filled
      - `Assignee` is set
      - `Labels` are set
      - `development` is linked if related issue exists
    
    ## Checklist (Right Before PR Review Request)
    - [x] The following has been handled:
      - `yarn inspect` is run
      - `TODOs` are handled and checked
      - Final Operation Check is done
      - Make this PR as an open PR
    
    ## Checklist (Reviewers)
    - [x] Check if there are any other missing TODOs that are not yet listed
    - [x] Review Code
    - [x] Every item on the checklist has been addressed accordingly
    - [x] If `development` is associated to this PR, you must check if every
    TODOs are handled

--- src/responses/get-user.res.ts
+++ src/responses/get-user.res.ts
@@ -1,5 +1,6 @@
 export interface GetSharedUserRes {
   imageUrl: null | string // null if user has no image or user does not want to show the image
+  displayingName: null | string
   firstName: null | string
   middleName: null | string
   lastName: null | string
