4a7db8be8c5c074a55cddc0501edc9482c01f099
AJ Kim <jkim67cloud@gmail.com>
2024-05-11 15:34:49 +0900

    feat: ok

--- src/domains/action/action-group.domain.ts
+++ src/domains/action/action-group.domain.ts
@@ -341,14 +341,35 @@ export class ActionGroupDomain extends DomainRoot {
         )
         continue
       }
-      //
-      // if action committed but is late, it is level 1:
-      // TODO: This part has a bug where if a day passes, if is not handled
-      if (this.props.closeAt.valueOf() < ad.createdAtValue) {
+      // if dummy, level 1:
+      if (ad.toResDTO(0).isDummy) {
         actionsDerived.push(ad.toResDTO(1))
         continue
       }
-      //
+      // dummy:
+      if (ad.isDummy) {
+        actionsDerived.push(ad.toResDTO(1))
+        continue
+      }
+
+      // if action committed but is late for that date, it is level 2:
+      // make sure to subtract this.props.closeAt.valueOf() from the days behind
+      // TODO: This is too complex, should have a good lambda or something.
+      const calculatedClosingDateFromToday =
+        this.props.closeAt.valueOf().valueOf() -
+        timeHandler.getStartOfToday(this.props.timezone).valueOf()
+      const [startOfDateThatAdCreatedAt] = timeHandler.getDateFromDaysAgo(
+        timeHandler.getDaysAgo(ad.createdAtValue, this.props.timezone),
+        this.props.timezone,
+      )
+      if (
+        calculatedClosingDateFromToday + startOfDateThatAdCreatedAt.valueOf() <
+        ad.createdAtValue
+      ) {
+        actionsDerived.push(ad.toResDTO(2))
+        continue
+      }
+
       // ideal: level 4
       actionsDerived.push(ad.toResDTO(4))
     }
