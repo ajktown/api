d523303328328ba49ddccb23f288fe97f81b1227
AJ Kim <jkim67cloud@gmail.com>
2024-05-20 06:24:16 +0900

    fix: rollback

--- src/controllers/shared-resource.controller.ts
+++ src/controllers/shared-resource.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req } from '@nestjs/common'
+import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common'
 import { AjkTownApiVersion } from './index.interface'
 import { PostSharedResourceDTO } from '@/dto/post-shared-resource.dto'
 import { AccessTokenDomain } from '@/domains/auth/access-token.domain'
@@ -11,6 +11,7 @@ import {
   SharedResourceModel,
   SharedResourceProps,
 } from '@/schemas/shared-resources.schema'
+import { GetSharedResourcesQueryDTO } from '@/dto/get-shared-resources-query.dto'
 
 export enum SharedResourceControllerPath {
   PostSharedResource = `shared-resource`,
@@ -39,5 +40,38 @@ export class SharedResourceController {
         dto,
       )
     ).toResDTO(this.wordModel, this.sharedResourceModel)
+  }
+
+  // TODO: Deprecated. delete me.
+  @Get(SharedResourceControllerPath.DeprecatedGetSharedResource)
+  async getSharedResource(
+    @Req() req: Request,
+    @Query() dto: GetSharedResourcesQueryDTO,
+  ) {
+    let nullableAtd: null | AccessTokenDomain = null
+    try {
+      nullableAtd = await AccessTokenDomain.fromReq(req, this.jwtService)
+    } catch {}
+
+    return (
+      await this.sharedResourceService.deprecatedGet(nullableAtd, dto)
+    ).toResDTO(this.wordModel, this.sharedResourceModel)
+  }
+
+  @Get(SharedResourceControllerPath.GetSharedResources)
+  async getSharedResources(
+    @Req() req: Request,
+    @Query() dto: GetSharedResourcesQueryDTO,
+  ) {
+    let nullableAtd: null | AccessTokenDomain = null
+    try {
+      nullableAtd = await AccessTokenDomain.fromReq(req, this.jwtService)
+    } catch {}
+
+    return {
+      sharedResources: (
+        await this.sharedResourceService.get(nullableAtd, dto)
+      ).map((e) => e.toResDTO(this.wordModel, this.sharedResourceModel)),
+    }
   }
 }
