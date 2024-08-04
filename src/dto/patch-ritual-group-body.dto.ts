da6a0b9c2d7f811ac58f03e2ae79bb034d11aa4f
AJ Kim <jkim67cloud@gmail.com>
2024-08-05 08:56:38 +0900

    feat: new isarchived

--- src/dto/patch-ritual-group-body.dto.ts
+++ src/dto/patch-ritual-group-body.dto.ts
@@ -1,6 +1,6 @@
 import { Transform } from 'class-transformer'
 import { IsArray, IsOptional } from 'class-validator'
-import { intoArray } from './index.validator'
+import { intoArray, intoBooleanOrUndefined } from './index.validator'
 
 // TODO: Name should be modifiable, but not yet supported, until multiple rituals are supported.
 export class PatchRitualGroupBodyDTO {
@@ -8,4 +8,8 @@ export class PatchRitualGroupBodyDTO {
   @IsOptional()
   @IsArray()
   actionGroupIds: string[]
+
+  @Transform(intoBooleanOrUndefined)
+  @IsOptional()
+  isArchived: undefined | boolean
 }
