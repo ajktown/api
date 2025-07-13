5ba03cbe67a12d3fc40cdf93dee3078ccf7e5d76
AJ Kim <jkim67cloud@gmail.com>
2025-07-14 07:43:35 +0900

    feat

--- src/domains/preference/index.interface.ts
+++ src/domains/preference/index.interface.ts
@@ -2,6 +2,7 @@ import { DataBasicsDate, GlobalLanguageCode } from 'src/global.interface'
 import { DictPreferenceDomain } from './index.dict.domain'
 import { AvailableDictCode } from '@/constants/available-dicts.const'
 
+// IPreference is the interface for user preferences
 export interface IPreference extends IPreferenceCGT, DataBasicsDate {
   id: string
   ownerId: string
@@ -11,6 +12,7 @@ export interface IPreference extends IPreferenceCGT, DataBasicsDate {
   gptApiKey: string
 }
 
+//IPreferenceCGT contains CGT (ConsistencyGPT Application) specific properties
 interface IPreferenceCGT {
   // If true, the user will see a progress dialog when the app is loading
   useProgressDialog: boolean
