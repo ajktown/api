9b29fc56024645cf1a1661332d78ca3e5cd9e9f8
AJ Kim <jkim67cloud@gmail.com>
2025-07-15 08:57:15 +0900

    feat

--- src/dto/patch-preference.dto.ts
+++ src/dto/patch-preference.dto.ts
@@ -1,6 +1,6 @@
 import { GlobalLanguageCode } from '@/global.interface'
 import { IsArray, IsBoolean, IsOptional, IsString } from 'class-validator'
-import { intoUniqueArray } from './index.validator'
+import { intoBoolean, intoBooleanOrUndefined, intoUniqueArray } from './index.validator'
 import { Transform } from 'class-transformer'
 
 /** PUT preference can be done only by the owner, and therefore does not require ownerId */
@@ -19,6 +19,7 @@ export class PatchPreferenceDto {
   @IsOptional()
   gptApiKey: string
 
+  @Transform(intoBooleanOrUndefined)
   @IsBoolean()
   @IsOptional()
   useProgressDialog: boolean
